fae1afb72ba821e00c281992c16cf21406e2a56b
ayush-bhagat02 <bhagataayush2k5gmail.com>
2025-09-07 15:20:11 +0530

    Todo completed...

---
+++ 10todo/src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id:1,
+            todo : "todo msg",
+            completed : false,
+        }
+
+    ],
+    addtodo : (todo)=>{},
+    updatetodo : (id, todo)=>{},
+    deletetodo : (id)=>{},
+    toggleComplete : (id)=>{}
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
